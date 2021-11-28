PQEnablers/android_kernel_realme_mt6785

drivers: misc: mediatek: Purge as much CMDQ_LOGs as possible
Signed-off-by: Vaisakh Murali <vaisakhmurali@gmail.com>
Signed-off-by: Vaisakh Murali <mvaisakh@statixos.com>

drivers/misc/mediatek/video/mt6853/cmdq/v3/cmdq_helper_ext.h
@@ -79,14 +79,14 @@ struct DumpFirstErrorStruct {
 
 #define CMDQ_LOG(string, args...) \
 do {			\
-	pr_notice("[CMDQ]"string, ##args); \
+	pr_debug("[CMDQ]"string, ##args); \
 	cmdq_core_save_first_dump("[CMDQ]"string, ##args); \
 } while (0)
 
 #define CMDQ_MSG(string, args...) \
 do {			\
 	if (cmdq_core_should_print_msg()) { \
-		pr_notice("[CMDQ]"string, ##args); \
+		pr_debug("[CMDQ]"string, ##args); \
 	} \
 } while (0)
 
@@ -100,7 +100,7 @@ do { \
 
 #define CMDQ_ERR(string, args...) \
 do {			\
-	pr_notice("[CMDQ][ERR]"string, ##args); \
+	pr_err("[CMDQ][ERR]"string, ##args); \
 	cmdq_core_save_first_dump("[CMDQ][ERR]"string, ##args); \
 } while (0)
 
@@ -116,7 +116,7 @@ if (status < 0)		\
 do {			\
 	char dispatchedTag[50]; \
 	snprintf(dispatchedTag, 50, "CRDISPATCH_KEY:%s", tag); \
-	pr_notice("[CMDQ][AEE]"string, ##args); \
+	pr_debug("[CMDQ][AEE]"string, ##args); \
 	cmdq_core_save_first_dump("[CMDQ][AEE]"string, ##args); \
 	cmdq_core_turnoff_first_dump(); \
 	aee_kernel_warning_api(__FILE__, __LINE__, \

PQEnablers/android_kernel_xiaomi_mt6768

drivers/misc/mediatek/cmdq/v3/cmdq_helper_ext.h
@@ -71,14 +71,14 @@ struct DumpFirstErrorStruct {
 
 #define CMDQ_LOG(string, args...) \
 do {			\
-	pr_notice("[CMDQ]"string, ##args); \
+	pr_debug("[CMDQ]"string, ##args); \
 	cmdq_core_save_first_dump("[CMDQ]"string, ##args); \
 } while (0)
 
 #define CMDQ_MSG(string, args...) \
 do {			\
 	if (cmdq_core_should_print_msg()) { \
-		pr_notice("[CMDQ]"string, ##args); \
+		pr_debug("[CMDQ]"string, ##args); \
 	} \
 } while (0)
 
@@ -92,7 +92,7 @@ do { \
 
 #define CMDQ_ERR(string, args...) \
 do {			\
-	pr_notice("[CMDQ][ERR]"string, ##args); \
+	pr_debug("[CMDQ][ERR]"string, ##args); \
 	cmdq_core_save_first_dump("[CMDQ][ERR]"string, ##args); \
 } while (0)
 
@@ -110,7 +110,7 @@ do {			\
 	int len = snprintf(dispatchedTag, 50, "CRDISPATCH_KEY:%s", tag); \
 	if (len >= 50) \
 		pr_debug("%s:%d len:%d over 50\n", __func__, __LINE__, len); \
-	pr_notice("[CMDQ][AEE]"string, ##args); \
+	pr_debug("[CMDQ][AEE]"string, ##args); \
 	cmdq_core_save_first_dump("[CMDQ][AEE]"string, ##args); \
 	cmdq_core_turnoff_first_dump(); \
 	aee_kernel_warning_api(__FILE__, __LINE__, \